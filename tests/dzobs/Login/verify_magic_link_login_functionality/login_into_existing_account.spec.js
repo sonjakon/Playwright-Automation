fa14db20f6ba52ee14c074d091505c08f6190abd
Sonja Koncar <koncar.sonja94@gmail.com>
2023-11-07 14:44:04 +0100

    Add test for magick link

--- tests/dzobs/Login/verify_magic_link_login_functionality/login_into_existing_account.spec.js
+++ tests/dzobs/Login/verify_magic_link_login_functionality/login_into_existing_account.spec.js
@@ -4,10 +4,16 @@ test('login into existing account', async ({ page }) => {
     await page.goto('https://www.dzobs.com/');
     await page.getByRole('button', { name: 'Prijava' }).click();
     await page.getByPlaceholder('Email').click();
-    await page.getByPlaceholder('Email').fill('test+success@magic.link');
+    await page.getByPlaceholder('Email').fill('koncar.sonja94@gmail.com');
     await page.getByRole('button', { name: 'Pošalji magični link' }).click();
-    await page.goto('https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox/FMfcgzGwHVStZXjkQpHKpXKpGRVKxjvg');
-    await page.getByText('Prijavi se').click();
 
-    await page.expect('https://www.dzobs.com/');
+    await page.goto('https://accounts.google.com/InteractiveLogin/signinchooser?continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&osid=1&passive=1209600&service=mail&ifkv=AVQVeyxM_FU2JmmmMyJ0yK0sVU8br3rV3CI4ZEEpBiUqgDJtRJCb9kEzut4Jpu8lM2w-dOGW8l2xWA&theme=glif&flowName=GlifWebSignIn&flowEntry=ServiceLogin');
+    await page.getByRole('link', { name: 'koncar.sonja94@gmail.com' }).click();
+    await page.locator('input[name="password"]').click();
+    await page.locator('input[name="password"]').fill('SonjaKoncar12*');
+    await page.getByRole('button', { name: 'Next' }).click();
+    page.locator('zA yO');
+    await page.getByRole('button', { name: 'Prijavi se' }).click();
+
+    await expect(page.getByText('Odjavi se')).toBeVisible();
 });
