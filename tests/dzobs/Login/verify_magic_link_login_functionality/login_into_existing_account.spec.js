3d900e04baf991116a19b8522e097c11fb7a15a3
Sonja Koncar <koncar.sonja94@gmail.com>
2023-11-16 14:30:19 +0100

    Izmjene sa oznacenim test steps

--- tests/dzobs/Login/verify_magic_link_login_functionality/login_into_existing_account.spec.js
+++ tests/dzobs/Login/verify_magic_link_login_functionality/login_into_existing_account.spec.js
@@ -3,29 +3,30 @@ import { test, expect } from '@playwright/test';
 test.slow();
 
 test('login into existing account', async ({ page }) => {
-    await page.goto('https://www.dzobs.com/');
-    await page.getByRole('button', { name: 'Prijava' }).click();
 
-    await page.waitForURL('https://www.dzobs.com/auth/signin?redirect=https://www.dzobs.com/');
+    await test.step("login to Dzobs", async () => {
+        await page.goto('https://www.dzobs.com/');
+        await page.getByRole('button', { name: 'Prijava' }).click();
+        await page.waitForURL('https://www.dzobs.com/auth/signin?redirect=https://www.dzobs.com/');
+        await page.getByPlaceholder('Email').click();
+        await page.getByPlaceholder('Email').fill('sonjatests@blondmail.com');
+        await page.getByRole('button', { name: 'Pošalji magični link' }).click();
+        await page.getByText('Login link poslan na email');
+    })
 
-    await page.getByPlaceholder('Email').click();
-    await page.getByPlaceholder('Email').fill('sonjatests@blondmail.com');
-    await page.getByRole('button', { name: 'Pošalji magični link' }).click();
-    await page.getByText('Login link poslan na email');
+    await test.step("login to Inbox", async () => {
+        await page.goto('https://inboxes.com/');
+        await page.getByRole('button', { name: 'Get my first inbox! arrow right' }).click();
+        await page.getByPlaceholder('jane1034').click();
+        await page.getByPlaceholder('jane1034').fill('sonjatests');
+        await page.getByRole('button', { name: 'Add Inbox' }).click();
+    })
 
-
-
-    await page.goto('https://inboxes.com/');
-    await page.getByRole('button', { name: 'Get my first inbox! arrow right' }).click();
-    await page.getByPlaceholder('jane1034').click();
-    await page.getByPlaceholder('jane1034').fill('sonjatests');
-    await page.getByRole('button', { name: 'Add Inbox' }).click();
-
-    await page.pause()
-
-    await page.getByRole('cell', { name: 'Prijavi se na Dzobs.com - [https://i.ibb.co/NWrW3GV/dzobs-logo-...' }).first().click();
-    await page.getByRole('link', { name: 'Prijavi se' }).click();
-    const page1Promise = page.waitForEvent('popup');
-    const page1 = await page1Promise;
-    await expect(page.getByRole('button', { name: 'Odjavi se' })).toBeVisible;
+    await test.step("open email received", async () => {
+        await page.getByRole('cell', { name: 'Prijavi se na Dzobs.com - [https://i.ibb.co/NWrW3GV/dzobs-logo-...' }).first().click();
+        await page.getByRole('link', { name: 'Prijavi se' }).click();
+        const page1Promise = page.waitForEvent('popup');
+        const page1 = await page1Promise;
+        await expect(page.getByRole('button', { name: 'Odjavi se' })).toBeVisible;
+    })
 });
