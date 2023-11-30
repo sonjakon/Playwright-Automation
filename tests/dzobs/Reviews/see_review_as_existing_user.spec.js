ff100ff0e41674276d8aa7d50e576fb68228f26a
Sonja Koncar <koncar.sonja94@gmail.com>
2023-11-30 12:54:23 +0100

    Dodane izmjene za 'see review as existing user'

--- tests/dzobs/Reviews/see_review_as_existing_user.spec.js
+++ tests/dzobs/Reviews/see_review_as_existing_user.spec.js
@@ -4,20 +4,11 @@ test.use({ storageState: 'tests/dzobs/auth/user.spec.json' });
 
 test('see review as existing user', async ({ page }) => {
 
-    test.slow();
+    await page.goto('https://www.dzobs.com/');
+    await page.locator('div.relative.group a button span:has-text("Kompanije")').hover();
+    await page.getByRole('link', { name: 'Pogledaj recenzije' }).click();
 
-    const spanElement = await page.$('div.relative.group a button span:has-text("Kompanije")');
-    if (spanElement) {
-        await spanElement.hover();
+    await expect(page.getByText('Recenzije možete vidjeti na profilima kompanija, ukoliko želite da ih vidite ovdje na jednom mjestu poredane hronološki, morate imati prethodno ostavljenu recenziju. Neki vid nagrade ekipi koja nam je pomogla da skupimo recenzije.')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Ostavi recenziju' })).toBeVisible;
 
-
-        const spanElement = await page.$('div.relative.group a span:has-text("Pogledaj recenzije")');
-        if (spanElement) {
-            await spanElement.click();
-        }
-
-
-        await expect(page.getByText('Recenzije možete vidjeti na profilima kompanija, ukoliko želite da ih vidite ovdje na jednom mjestu poredane hronološki, morate imati prethodno ostavljenu recenziju. Neki vid nagrade ekipi koja nam je pomogla da skupimo recenzije.')).toBeVisible();
-    }
 });
-
