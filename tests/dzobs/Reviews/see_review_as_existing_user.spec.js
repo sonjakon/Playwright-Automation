75f23b1025495c7ef4982fc254bc9d6501979dc7
Sonja Koncar <koncar.sonja94@gmail.com>
2023-11-30 15:27:12 +0100

    Finalne izmjene za 'see review as existing user'

--- tests/dzobs/Reviews/see_review_as_existing_user.spec.js
+++ tests/dzobs/Reviews/see_review_as_existing_user.spec.js
@@ -4,11 +4,19 @@ test.use({ storageState: 'tests/dzobs/auth/user.spec.json' });
 
 test('see review as existing user', async ({ page }) => {
 
-    await page.goto('https://www.dzobs.com/');
-    await page.locator('div.relative.group a button span:has-text("Kompanije")').hover();
-    await page.getByRole('link', { name: 'Pogledaj recenzije' }).click();
+    await test.step("go to review's page", async () => {
+        await page.goto('https://www.dzobs.com/');
+        await page.locator('div.relative.group a button span:has-text("Kompanije")').hover();
+        await page.getByRole('link', { name: 'Pogledaj recenzije' }).click();
+    })
 
-    await expect(page.getByText('Recenzije možete vidjeti na profilima kompanija, ukoliko želite da ih vidite ovdje na jednom mjestu poredane hronološki, morate imati prethodno ostavljenu recenziju. Neki vid nagrade ekipi koja nam je pomogla da skupimo recenzije.')).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Ostavi recenziju' })).toBeVisible;
+    await test.step("verify the page", async () => {
+        await expect(page.locator('lg:px-0 w-full m-auto mb-8')).toBeHidden();
+        await expect(page.getByRole('button', { name: 'Ostavi recenziju' })).toBeVisible();
+    })
 
+    await test.step("verify button functionality", async () => {
+        await page.getByRole('button', { name: 'Ostavi recenziju' }).click();
+        await expect(page.getByRole('button', { name: 'Pošalji' })).toBeVisible();
+    })
 });
